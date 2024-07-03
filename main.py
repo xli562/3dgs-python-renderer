344e7e6d1c54f90f7ebec1e334eb4886ef923e9e
Xiaoyang (Henry) Li <xl562@cam.ac.uk>
2024-07-03 15:41:49 +0100

    modified file structure

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from utils.gaussian_utils import *
-from utils.file_utils import *
 from utils.plotting_utils import *
+from utils.graphics_utils import *
 from utils.camera_utils import *
 
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-
-
-model = load_gau_from_ply(r'D:\MyCodes\MyPythonCodes\3dgs\3dgs-acceleration\output\chair\point_cloud\iteration_7000\point_cloud.ply', 
-                          10000)
-
 print('Loading gaussians ...')
+model:GaussianData = load_gau_from_ply(r'D:\MyCodes\MyPythonCodes\3dgs\3dgs-acceleration\output\chair\point_cloud\iteration_7000\point_cloud.ply', 
+                          1000)
 gaussian_objects = []
-for (pos, scale, rot, opacity, sh) in tqdm(zip(model.xyz, model.scale, model.rot, model.opacity, model.sh)):
+for (pos, scale, rot, opacity, sh) in tqdm(zip(model.xyz, model.scale, model.rot, model.opacity, model.sh), smoothing=0.6):
     gaussian_objects.append(Gaussian(pos, scale, rot, opacity, sh))
 
-h = 720
-w = 1080
-# camera = Camera(h, w, position=(0.94, 1.29, -2.65), target=(0.0, 0.66, 0.07))
-camera = Camera(h, w, position=(-0.57651054, 2.99040512, -0.03924271), target=(-0.0, 0.0, 0.0))
+h, w = 720, 1080
+camera = Camera(h, w, position=(0.94, 1.29, -2.65), target=(0.0, 0.66, 0.07))
+# camera = Camera(h, w, position=(-0.57651054, 2.99040512, -0.03924271), target=(-0.0, 0.0, 0.0))
 
 bitmap = plot_model(camera, gaussian_objects)
 
---
+++ utils/graphics_utils.py
@@ -0,0 +1,79 @@
+import numpy as np
+from tqdm import tqdm
+from utils.gaussian_utils import Gaussian
+from utils.camera_utils import Camera
+
+
+def plot_opacity(gaussian: Gaussian, camera: Camera, w: int, h: int, bitmap: np.ndarray, alphas: np.ndarray):
+    """ Compute the opacity of a gaussian given the camera """
+    conic, bboxsize_cam, bbox_ndc = gaussian.get_conic_and_bb(camera)
+
+    A, B, C = conic
+
+    screen_height, screen_width = bitmap.shape[:2]
+    bbox_screen = camera.ndc_to_pixel(bbox_ndc, screen_width, screen_height)
+    
+    if np.any(np.isnan(bbox_screen)):
+        return
+
+    ul = bbox_screen[0,:2]
+    ur = bbox_screen[1,:2]
+    lr = bbox_screen[2,:2]
+    ll = bbox_screen[3,:2]
+    
+    y1 = int(np.floor(ul[1]))
+    y2 = int(np.ceil(ll[1]))
+    
+    x1 = int(np.floor(ul[0]))
+    x2 = int(np.ceil(ur[0]))
+    nx = x2 - x1
+    ny = y2 - y1
+
+    # Extract out inputs for the gaussian
+    coordxy = bboxsize_cam
+    x_cam_1 = coordxy[0][0]   # ul
+    x_cam_2 = coordxy[1][0]   # ur
+    y_cam_1 = coordxy[1][1]   # ur (y)
+    y_cam_2 = coordxy[2][1]   # lr
+
+    camera_dir = gaussian.pos - camera.position
+    camera_dir = camera_dir / np.linalg.norm(camera_dir)
+    color = gaussian.get_color(camera_dir)
+
+    for x, x_cam in zip(range(x1, x2), np.linspace(x_cam_1, x_cam_2, nx)):
+        if x < 0 or x >= w:
+            continue
+        for y, y_cam in zip(range(y1, y2), np.linspace(y_cam_1, y_cam_2, ny)):
+            if y < 0 or y >= h:
+                continue
+
+            # Gaussian is typically calculated as f(x, y) = A * exp(-(a*x^2 + 2*b*x*y + c*y^2))
+            power = -(A*x_cam**2 + C*y_cam**2)/2.0 - B * x_cam * y_cam
+            if power > 0.0:
+                continue
+
+            alpha = gaussian.opacity * np.exp(power)
+            alpha = min(0.99, alpha)
+            if gaussian.opacity < 1.0 / 255.0:
+                continue
+
+            # Set the pixel color to the given color and opacity
+            # Do alpha blending using "over" method
+            old_alpha = alphas[y, x]
+            new_alpha = alpha + old_alpha * (1.0 - alpha)
+            alphas[y, x] = new_alpha
+            bitmap[y, x, :] = (color[0:3]) * alpha + bitmap[y, x, :] * (1.0 - alpha)
+
+
+def plot_model(camera, gaussian_objects):
+    print('Sorting the gaussians by depth')
+    indices = np.argsort([gau.get_depth(camera) for gau in gaussian_objects])
+    
+    print('Plotting with', len(gaussian_objects), 'gaussians')
+    bitmap = np.zeros((camera.h, camera.w, 3), np.float32)
+    alphas = np.zeros((camera.h, camera.w), np.float32)
+    
+    for idx in tqdm(indices):
+        plot_opacity(gaussian_objects[idx], camera, camera.w, camera.h, bitmap, alphas)
+    
+    return bitmap
