6039dc0596abb607ea3ece14cb62eaa44cd78621
Xiaoyang (Henry) Li <xl562@cam.ac.uk>
2024-07-15 16:23:01 +0100

    added flag support to main to run with args

--- main.py
+++ main.py
@@ -3,25 +3,31 @@ from utils.plotting_utils import *
 from utils.graphics_utils import *
 from utils.camera_utils import *
 
-import os
+import os, argparse
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
 ON_LINUX = os.name == 'posix'
 PLY_PATH = r'D:\MyCodes\MyPythonCodes\3dgs\3dgs-acceleration\output\chair\point_cloud\iteration_7000\point_cloud.ply' if not ON_LINUX else '/home/xl562/chair_point_cloud.ply'
 
-print('Loading gaussians ...')
-model:GaussianData = load_gau_from_ply(PLY_PATH, -1)
-gaussian_objects = []
-for (pos, scale, rot, opacity, sh) in tqdm(zip(model.xyz, model.scale, model.rot, model.opacity, model.sh), smoothing=0.6):
-    gaussian_objects.append(Gaussian(pos, scale, rot, opacity, sh))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='3D Gaussian Renderer')
+    parser.add_argument('-i', type=int, required=False, default=1000, help='sample size from original point cloud, default=1000, complete sampling: -1')
+    args = parser.parse_args()
+    sample_size = args.i
 
-h, w = 720, 1080
-camera = Camera(h, w, position=(0.94, 1.29, -2.65), target=(0.0, 0.66, 0.07))
-# camera = Camera(h, w, position=(-0.57651054, 2.99040512, -0.03924271), target=(-0.0, 0.0, 0.0))
+    print('Loading gaussians ...')
+    model:GaussianData = load_gau_from_ply(PLY_PATH, sample_size)
+    gaussian_objects = []
+    for (pos, scale, rot, opacity, sh) in tqdm(zip(model.xyz, model.scale, model.rot, model.opacity, model.sh), smoothing=0.6):
+        gaussian_objects.append(Gaussian(pos, scale, rot, opacity, sh))
 
-bitmap = gau_to_bitmap(camera, gaussian_objects)
+    h, w = 720, 1080
+    camera = Camera(h, w, position=(0.94, 1.29, -2.65), target=(0.0, 0.66, 0.07))
+    # camera = Camera(h, w, position=(-0.57651054, 2.99040512, -0.03924271), target=(-0.0, 0.0, 0.0))
 
-plt.figure(figsize=(12, 12))
-plt.imshow(bitmap, vmin=0, vmax=1.0)
-plt.show()
+    bitmap = gau_to_bitmap(camera, gaussian_objects)
+
+    plt.figure(figsize=(12, 12))
+    plt.imshow(bitmap, vmin=0, vmax=1.0)
+    plt.show()
